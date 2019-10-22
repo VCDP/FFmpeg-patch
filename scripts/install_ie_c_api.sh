b859de488edb7bc9622a94164d6709150da4ecc2
Lin Xie <lin.xie@intel.com>
2019-10-22 14:39:22 +0800

    fix install c api script

--- scripts/install_ie_c_api.sh
+++ scripts/install_ie_c_api.sh
@@ -8,7 +8,7 @@
 CURDIR=$PWD
 cd /tmp/
 
-tar -zxvf ../thirdparty/dldt-c-api/source/dldt-c_api_v2-1.0.tar.gz && \
+tar -zxvf $CURDIR/../thirdparty/dldt-c-api/source/dldt-c_api_v2-1.0.tar.gz && \
     cd dldt-c_api-1.0 && \
     mkdir -p build && cd build && \
     cmake -DENABLE_AVX512F=OFF .. && \
