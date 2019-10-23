525f337c0d89d962b917122cbbd87c928cc68c61
Lin Xie <lin.xie@intel.com>
2019-10-23 11:57:00 +0800

    update script

--- scripts/build_install_libva_media_driver.sh
+++ scripts/build_install_libva_media_driver.sh
@@ -7,6 +7,9 @@
 
 sudo -E apt-get install libpciaccess-dev autoconf libtool
 
+CURDIR=$PWD
+cd /tmp/
+
 #unset LIBVA_DRIVERS_PATH
 #unset LIBVA_DRIVER_NAME
 
@@ -26,3 +29,9 @@ git clone  https://github.com/intel/libva-utils.git
 cd libva-utils
 ./autogen.sh && make && sudo make install
 cd ..
+
+wget -O - https://github.com/Intel-Media-SDK/MediaSDK/releases/download/intel-mediasdk-19.1.0/MediaStack.tar.gz | tar xz
+cd MediaStack && sudo ./install_media.sh
+cd ..
+
+cd $CURDIR
