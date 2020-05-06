4e4205a8df4c71dae79ecb36145becf163c5d927
Lin Xie <lin.xie@intel.com>
2020-05-06 11:57:31 +0800

    update build scripts

--- scripts/build_ffmpeg-dynamic.sh
+++ scripts/build_ffmpeg-dynamic.sh
@@ -8,9 +8,12 @@
 # If you use MSS install package, please add following VAR into ENV
 LIBVA_PKG_CONFIG_PATH="/opt/intel/mediasdk/lib64/pkgconfig"
 PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:$LIBVA_PKG_CONFIG_PATH"
+OPENVIO_IE_PATH="/opt/intel/openvino/inference_engine"
 
 ./configure \
     --extra-libs="-lpthread -lm" \
+    --extra-cflags="-I$OPENVIO_IE_PATH/include" \
+    --extra-ldflags="-L$OPENVIO_IE_PATH/lib/intel64" \
     --enable-shared \
     --enable-gpl    \
     --enable-openssl \
@@ -21,7 +24,7 @@ PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:$LIBVA_PKG_CONFIG_PATH"
     --enable-libx265 \
     --enable-librdkafka \
     --enable-libjson_c \
-    --enable-libinference_engine_c_wrapper
+    --enable-libinference_engine_c_api
 # appending options here to enable more third party components
 
 make -j16 && make install
